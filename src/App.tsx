2c39b46bd9ecfc9082f1777f0660316f427ee962
Henri Valette <henri.valette.fr@gmail.com>
2024-03-16 18:56:07 +0100

    fix: rect rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,64 @@
-import { Svg, SVG } from "@svgdotjs/svg.js";
-import "@svgdotjs/svg.panzoom.js";
-import { useEffect } from "react";
+import { Svg, SVG } from '@svgdotjs/svg.js';
+import '@svgdotjs/svg.panzoom.js';
+import { useEffect } from 'react';
 
 function App() {
   useEffect(() => {
-    const svg = SVG("#svg").addTo("body") as Svg;
-    // svg.panZoom(); // does not work :-(
+    const svg = SVG('#svg').addTo('body') as Svg;
+    svg.panZoom({
+      panButton: 1,
+    });
 
-    SVG("#group")?.remove();
+    SVG('#group')?.remove();
 
-    const group = svg.group().attr("id", "group");
+    const group = svg.group().attr('id', 'group');
 
     const rect = group
       .rect(200, 200)
       .move(200, 200)
       .rotate(0)
-      .fill("#f06")
-      .transform({ origin: "center center" });
+      .fill('#f06')
+      .transform({ origin: 'center center' });
 
     const circle = group
       .circle(30)
       .move(rect.x(), rect.y())
       .translate(-15, -15)
-      .fill("#fff")
-      .css("cursor", "grab")
-      .attr("id", "circle");
+      .fill('#fff')
+      .css('cursor', 'grab')
+      .attr('id', 'circle');
 
     let rotating = false;
 
-    svg.on("mousedown", (e) => {
+    svg.on('mousedown', (e) => {
       const event = e as MouseEvent;
       if (event.target === circle.node) {
         rotating = true;
       }
     });
 
-    svg.on("mouseup", () => {
+    svg.on('mouseup', () => {
       rotating = false;
     });
 
-    svg.on("mousemove", (e) => {
+    svg.on('mousemove', (e) => {
       if (rotating) {
         const event = e as MouseEvent;
 
-        const clientRect = svg.node.getBoundingClientRect();
-        const mouse = {
-          x: Math.round(event.clientX - clientRect.left),
-          y: Math.round(event.clientY - clientRect.top),
-        };
+        const pt = svg.node.createSVGPoint();
+        pt.x = event.clientX;
+        pt.y = event.clientY;
+        const mouse = pt.matrixTransform(svg.node.getScreenCTM()?.inverse());
 
         const A = Math.atan2(
           rect.node.getBoundingClientRect().height / 2,
-          rect.node.getBoundingClientRect().width / 2,
+          rect.node.getBoundingClientRect().width / 2
         );
 
         const a =
           Math.atan2(
-            rect.attr("y") + rect.attr("height") / 2 - mouse.y,
-            rect.attr("x") + rect.attr("width") / 2 - mouse.x,
+            rect.attr('y') + rect.attr('height') / 2 - mouse.y,
+            rect.attr('x') + rect.attr('width') / 2 - mouse.x
           ) - A;
 
         group.transform({
@@ -67,8 +68,7 @@ function App() {
     });
   }, []);
 
-  return <svg id="svg" width="650" height="600" viewBox="0 0 650 600"></svg>; // works!
-  // return <svg id="svg" width="650" height="600" viewBox="0 0 1000 600"></svg>; // does not work :-(
+  return <svg id="svg" width="100%" height="100%" viewBox="0 0 1000 600"></svg>; // does not work :-(
 }
 
 export default App;
