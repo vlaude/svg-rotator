150613aa4436543e31e92ff84055529c76a9b137
vlaude <vincent.laude2@gmail.com>
2024-03-17 16:53:27 +0100

    wipee wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,53 @@
-import { Svg, SVG } from '@svgdotjs/svg.js';
-import '@svgdotjs/svg.panzoom.js';
-import { useEffect } from 'react';
+import { Svg, SVG } from "@svgdotjs/svg.js";
+import "@svgdotjs/svg.panzoom.js";
+import "@svgdotjs/svg.draggable.js";
+import React, { useCallback, useMemo } from "react";
+import { ReactSVG, Props as ReactSVGProps } from "react-svg";
+import svg from "./assets/svg.svg";
+
+const ReactSVGMemo = React.memo(ReactSVG);
 
 function App() {
-  useEffect(() => {
-    const svg = SVG('#svg').addTo('body') as Svg;
-    svg.panZoom({
-      panButton: 1,
-    });
+  const initSvg = useCallback(() => {
+    const svg = SVG("#svg").size("100%", "100%") as Svg;
 
-    SVG('#group')?.remove();
-
-    const group = svg.group().attr('id', 'group');
+    SVG("#group")?.remove();
+    const group = svg
+      .group()
+      .attr("id", "group")
+      .css("cursor", "pointer")
+      .draggable();
 
     const rect = group
       .rect(200, 200)
       .move(200, 200)
       .rotate(0)
-      .fill('#f06')
-      .transform({ origin: 'center center' });
+      .fill("#f06")
+      .transform({ origin: "center center" });
 
     const circle = group
       .circle(30)
       .move(rect.x(), rect.y())
       .translate(-15, -15)
-      .fill('#fff')
-      .css('cursor', 'grab')
-      .attr('id', 'circle');
+      .fill("#fff")
+      .css("cursor", "grab")
+      .attr("id", "circle");
 
     let rotating = false;
 
-    svg.on('mousedown', (e) => {
-      const event = e as MouseEvent;
-      if (event.target === circle.node) {
-        rotating = true;
+    circle.on("mousedown", () => {
+      rotating = true;
+      group.draggable(false);
+    });
+
+    svg.on("mouseup", () => {
+      if (rotating) {
+        rotating = false;
+        group.draggable();
       }
     });
 
-    svg.on('mouseup', () => {
-      rotating = false;
-    });
-
-    svg.on('mousemove', (e) => {
+    svg.on("mousemove", (e) => {
       if (rotating) {
         const event = e as MouseEvent;
 
@@ -52,13 +58,13 @@ function App() {
 
         const A = Math.atan2(
           rect.node.getBoundingClientRect().height / 2,
-          rect.node.getBoundingClientRect().width / 2
+          rect.node.getBoundingClientRect().width / 2,
         );
 
         const a =
           Math.atan2(
-            rect.attr('y') + rect.attr('height') / 2 - mouse.y,
-            rect.attr('x') + rect.attr('width') / 2 - mouse.x
+            rect.attr("y") + rect.attr("height") / 2 - mouse.y,
+            rect.attr("x") + rect.attr("width") / 2 - mouse.x,
           ) - A;
 
         group.transform({
@@ -68,7 +74,28 @@ function App() {
     });
   }, []);
 
-  return <svg id="svg" width="100%" height="100%" viewBox="0 0 1000 600"></svg>; // does not work :-(
+  const memoValue: Omit<ReactSVGProps, "ref"> = useMemo(
+    () => ({
+      src: svg,
+      className: "svg-wrapper",
+      wrapper: "div",
+      loading: () => <div>Chargement...</div>,
+      beforeInjection: (svg) => {
+        svg.setAttribute("id", "svg");
+      },
+      afterInjection: () => {
+        console.log("SVG loaded successfully");
+        initSvg();
+      },
+      onError: (error) => {
+        console.log("Error loading SVG");
+        console.error(error);
+      },
+    }),
+    [initSvg],
+  );
+
+  return <ReactSVGMemo {...memoValue} />;
 }
 
 export default App;
